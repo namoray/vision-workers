f12281807a4d8768e68a6035e9711b5e0c4911d5
namoray <neuvaamor@proton.me>
2024-04-09 09:39:35 +0000

    autoupdate fix

--- run_autoupdater.py
+++ run_autoupdater.py
@@ -14,10 +14,13 @@ def run_autoupdate(restart_script: str, process_pid: int):
 
     while True:
         local_tag = subprocess.getoutput("git describe --abbrev=0 --tags")
-        os.system(f"git fetch origin {branch_name}")
+        subprocess.run(["git fetch"], shell=True)
         remote_tag = subprocess.getoutput(
             f"git describe --tags `git rev-list --tags=origin/{branch_name} --max-count=1`"
         )
+        print("branch nam:", branch_name)
+        print("localtag:", local_tag)
+        print("remotetag:", remote_tag)
 
         if should_update_local(local_tag, remote_tag):
             print("Local repo is not up-to-date. Updating...")
@@ -32,15 +35,15 @@ def run_autoupdate(restart_script: str, process_pid: int):
 
                 print("Running the autoupdate steps...")
                 # Trigger shell script. Make sure this file path starts from root
-                subprocess.run(["kill", f"{process_pid}"], shell=True)
-                subprocess.run(["wait", f"{process_pid}", "2>/dev/null"], shell=True)
+                subprocess.run(["kill {process_pid}"], shell=True)
+                subprocess.run(["wait {process_pid} 2>/dev/null"], shell=True)
                 subprocess.call([f"./{restart_script}"], shell=True)
                 print("Finished running the autoupdate steps! Ready to go 😎")
 
         else:
             print("Repo is up-to-date.")
-
-        time.sleep(60)
+        # Change after dev
+        time.sleep(5)
 
 
 if __name__ == "__main__":
@@ -50,6 +53,4 @@ if __name__ == "__main__":
     parser.add_argument("--restart_script", type=str)
     parser.add_argument("--process_pid", type=int)
     args = parser.parse_args()
-    run_autoupdate(
-        restart_script=args.restart_script, process_pid=args.process_pid
-    )
+    run_autoupdate(restart_script=args.restart_script, process_pid=args.process_pid)
