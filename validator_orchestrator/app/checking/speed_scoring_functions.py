944145d8eb49f59fb2a515766adaefac22e1d19d
namoray <neuvaamor@proton.me>
2024-04-12 15:53:21 +0000

    Avatar overhead and image server docker fixes

--- validator_orchestrator/app/checking/speed_scoring_functions.py
+++ validator_orchestrator/app/checking/speed_scoring_functions.py
@@ -19,6 +19,7 @@ def _calculate_speed_modifier(
     )
     return max(1 - penalty**2, 0)
 
+
 def _calculate_work_bonus_images(
     steps: int, overhead: float, lower_bound_for_seconds_per_step: float
 ):
