f6eea02f423ca3f574f590393ce42737214e1feb
Arpan Tripathi <tripathiarpan20@gmail.com>
2024-11-25 14:18:34 +0000

    Update text_based.py

--- validator_orchestrator/app/checking/functions/text_based.py
+++ validator_orchestrator/app/checking/functions/text_based.py
@@ -137,3 +137,80 @@ async def check_text_result(result: models.QueryResult, payload: dict, task_conf
         return 0
     score = _score_average_distance(average_distance)
     return score
+
+async def check_text_comp_result(result: models.QueryResult, payload: dict, task_config: models.OrchestratorServerConfig) -> Union[float, None]:
+    formatted_response = json.loads(result.formatted_response) if isinstance(result.formatted_response, str) else result.formatted_response
+    messages: list[models.MessageResponse] = []
+    for response in formatted_response:
+        try:
+            content = response["choices"][0]["text"]
+            logprobs = response["choices"][0]["logprobs"]
+            # Below is a fix for the first message not having logprobs
+            if content == "" and logprobs is None:
+                role = response["choices"][0]["text"]
+                if role == "assistant":
+                    continue
+            logprob = logprobs["token_logprobs"][0]
+            messages.append(models.MessageResponse(role="assistant", content=content, logprob=logprob))
+        except Exception as e:
+            logger.error(f"Error with logprob: {e}. Response: {response}")
+            return 0  # Important to return 0 as this is a critical error
+
+    # If no responses, then not a good response
+    if len(messages) == 0:
+        return 0
+
+    if len(messages) == 1:
+        indicies_to_check = [0]
+    else:
+        # Always check first & last
+        indicies_to_check = [0, len(messages) - 1]
+        number_of_additional_indicies_to_check = len(messages) - 2
+        additional_indicies_to_check = random.sample(
+            range(1, len(messages) - 1),
+            number_of_additional_indicies_to_check,
+        )
+        indicies_to_check.extend(additional_indicies_to_check)
+
+    total_distance = 0
+    checks = 0
+
+    payload["starting_assistant_message"] = True
+    payload["number_of_logprobs"] = 5
+    payload["top_k"] = 5
+
+    llm_request = models.ChatRequestModel(**payload)
+    llm_request.max_tokens = 1
+
+    for index in indicies_to_check:
+        if checks >= 5:
+            continue
+
+        if index == 0:
+            llm_request.starting_assistant_message = True
+        else:
+            llm_request.starting_assistant_message = False
+
+            text_to_inject_into_assistant_message = "".join([i.content for i in messages[:index]])
+            llm_request.messages.append(
+                models.Message(
+                    **{
+                        "role": "assistant",
+                        "content": text_to_inject_into_assistant_message,
+                    }
+                )
+            )
+
+        distance = await _calculate_distance_for_token(task_config, llm_request, messages, index)
+        checks += 1
+        total_distance += distance
+        if index != 0:
+            llm_request.messages = llm_request.messages[:-1]
+
+    try:
+        average_distance = total_distance / checks
+    except Exception as e:
+        logger.error(f"Error with average distance: {e}. Total distance: {total_distance}. Checks: {checks}")
+        return 0
+    score = _score_average_distance(average_distance)
+    return score
