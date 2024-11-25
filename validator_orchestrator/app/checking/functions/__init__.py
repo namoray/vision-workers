892fbb5706477b59be9404fa0a54f3555d464f62
Arpan Tripathi <tripathiarpan20@gmail.com>
2024-11-25 14:20:41 +0000

    Update __init__.py

--- validator_orchestrator/app/checking/functions/__init__.py
+++ validator_orchestrator/app/checking/functions/__init__.py
@@ -1,5 +1,5 @@
 from app.checking.functions.image_based import check_image_result
-from app.checking.functions.text_based import check_text_result
+from app.checking.functions.text_based import check_text_result, check_text_comp_result
 
 
-__all__ = ["check_text_result", "check_image_result"]
+__all__ = ["check_text_result", "check_text_comp_result", "check_image_result"]
