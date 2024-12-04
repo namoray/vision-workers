9a059c7b28b2fcb37251fae7665eee7b1279e998
namoray <neuvaamor@proton.me>
2024-12-04 14:22:56 +0100

    dont build llms

--- build-docker-ci-remote.sh
+++ build-docker-ci-remote.sh
@@ -22,10 +22,8 @@ if [ "$disk_usage" -gt 50 ]; then
     docker image ls --format "{{.Repository}}:{{.Tag}} {{.CreatedAt}}" | grep "${IMAGE_PREFIX}/cicd" | sort -rk2 > /tmp/docker_images.txt
     latest_orchestrator=$(grep 'orchestrator-' /tmp/docker_images.txt | head -n 1 | awk '{print $1}')
     latest_image_server=$(grep 'image-server-' /tmp/docker_images.txt | head -n 1 | awk '{print $1}')
-    latest_llm_server=$(grep 'llm-server-' /tmp/docker_images.txt | head -n 1 | awk '{print $1}')
     echo $latest_orchestrator >> /tmp/keep_images.txt
     echo $latest_image_server >> /tmp/keep_images.txt
-    echo $latest_llm_server >> /tmp/keep_images.txt
     echo 'nvidia/cuda:11.8.0-devel-ubuntu20.04' >> /tmp/keep_images.txt
     cat /tmp/docker_images.txt | grep -v -F -f /tmp/keep_images.txt | awk '{print $1}' | xargs -r docker rmi -f
     docker system prune -f
